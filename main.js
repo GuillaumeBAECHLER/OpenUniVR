38ec9918f5b1646a6065274462343be632f02a37
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2019-05-19 22:19:13 +0200

    making user move

--- main.js
+++ main.js
@@ -5,6 +5,33 @@ import App from './src/App.vue'
 import VueRouter from 'vue-router'
 import routes from './src/router'
 
+AFRAME.registerComponent('show-clickable-on-hover', {
+  init: function () {
+    this.el.addEventListener('model-loaded', () => {
+      const obj = this.el.getObject3D('mesh')
+      obj.children.forEach((node) => {
+        node.el.addEventListener('mouseenter', () => {
+          node.material.emissive.set('white')
+        })
+        node.el.addEventListener('mouseleave', () => {
+          node.material.emissive.set('#000')
+        })
+        node.material.emissiveIntensity = 0.1
+      })
+      /* obj.traverse(node => {
+        console.log(node)
+        node.el.addEventListener('mouseenter', () => {
+          node.material.emissive.set('white')
+        })
+        node.el.addEventListener('mouseleave', () => {
+          node.material.emissive.set('#000')
+        })
+        node.material.emissiveIntensity = 0.5
+      }) */
+    })
+  }
+})
+
 const router = new VueRouter({
   routes // short for `routes: routes`
 })
--- src/scenes/University.vue
+++ src/scenes/University.vue
@@ -1,29 +1,50 @@
 <template>
-  <a-scene shadow="type: pcfsoft">
+  <a-scene>
     <div class="ui-panel">
+      <p v-show="showloader">déplacement en cours...</p>
       <users/>
     </div>
     <a-assets>
       <img id="sky" src="../assets/images/sky.jpg">
     </a-assets>
     <a-sky src="#sky"></a-sky>
-    <a-camera fov="60">
-    </a-camera>
+    <!-- <a-entity gltf-model="#university" rotation="0 180 0" material="color: white" modify-materials></a-entity>
+    -->
+    <a-entity :gltf-model="university_wall" rotation="0 180 0">
+      <a-entity :gltf-model="university_doors" show-clickable-on-hover></a-entity>
+    </a-entity>
     <a-entity light="type: directional; color: #FFF; intensity: 1.4; castShadow:true;" position="-1 1 1"></a-entity>
-    <a-box @mousedown="changeColor" shadow="receive: true" height="2" width="2" depth="2" position="0 1 -5" rotation="0 0 0" :color="color"></a-box>
-    <a-box @mousedown="changeColor" shadow="receive: true" height="2" width="2" depth="2" position="3 1 -5" rotation="0 45 0" :color="color"></a-box>
-    <a-plane shadow="receive: true" color="#59CD90" height="100" width="100" rotation="-90 0 0"></a-plane>
-    <a-entity @trackpaddown="changeColor" laser-controls="hand: right"></a-entity>
+    <a-plane shadow="receive: true" color="#59CD90" height="25" width="25" rotation="-90 0 0" @mousedown="initiateMove" @mouseup="cancelMove"></a-plane>
+    <a-circle 
+      ref="user_position"
+      position= "0 0.001 0"
+      rotation="-90 0 0"
+      animation="startEvents: moveuser; pauseEvents: moveusercancelled; property: scale; to: 0 0 0; dur: 1000; easing: linear"
+    ></a-circle>
+    <a-entity ref="user">
+      <a-camera fov="60" ref="camera">
+        <a-cursor></a-cursor>
+      </a-camera>
+      <a-entity @trackpaddown="changeColor" laser-controls="hand: right">
+      </a-entity>
+    </a-entity>
   </a-scene>
 </template>
 
 <script>
 import Users from '../components/Users.vue'
+const assets = {
+  university_wall: require('../assets/university.glb'),
+  university_doors: require('../assets/university-doors.glb'),
+}
 
 export default {
   name: 'University',
   data: () => ({
-    color: '#121212'
+    color: '#121212',
+    moving: null,
+    showloader: false,
+    ...assets
   }),
   components: {
     Users
@@ -39,6 +60,46 @@ export default {
     },
     changeColor: function () {
       this.color = this.getRandomColor()
+    },
+    initiateMove: function(evt) {
+      const vm = this
+      const moveto = evt.detail.intersection.point
+      vm.showloader = true
+      vm.$refs.user_position.setAttribute(
+        'position', 
+        {
+        ...moveto,
+        y: vm.$refs.user_position.getAttribute('position').y
+        }
+      )
+      vm.$refs.user_position.dispatchEvent(new Event('moveuser'));
+      this.moving = setTimeout(function(){
+        vm.showloader = false
+        vm.$refs.user.setAttribute(
+          'position', 
+          {
+          ...moveto,
+          y: vm.$refs.user.getAttribute('position').y
+          }
+        )
+      }, 1000)
+    },
+    cancelMove: function() {
+      clearTimeout(this.moving)
+      this.showloader = false
+      this.$refs.user_position.dispatchEvent(new Event('moveusercancelled'))
+      this.$refs.user_position.setAttribute(
+        'scale', {
+          x: 1, y: 1, z: 1
+        }
+      )
+      this.$refs.user_position.setAttribute(
+        'position', 
+        {
+        ...(this.$refs.user.getAttribute('position')),
+        y: this.$refs.user_position.getAttribute('position').y
+        }
+      )
     }
   }
 }
